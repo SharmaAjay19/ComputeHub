18df3af87704e01fd25e1c43b83c72911674f54a
Ajay Sharma <ajsharm@microsoft.com>
2017-11-11 21:15:26 +0530

    Imported WiFiP2P in main activity

--- ComputeHub/app/src/main/java/com/ajsharm/computehub/LandingPage.java
+++ ComputeHub/app/src/main/java/com/ajsharm/computehub/LandingPage.java
@@ -1,13 +1,28 @@
 package com.ajsharm.computehub;
 
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LandingPage extends AppCompatActivity {
 
+    private final IntentFilter intentFilter = new IntentFilter();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
     }
 }
